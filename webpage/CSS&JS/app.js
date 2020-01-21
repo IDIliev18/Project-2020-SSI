2e0cd1a3c259a06a2df3943e6185c538cf025c2e
Илиян Димитров Илиев <IDIliev18@codingburgas.bg>
2020-01-21 14:44:37 +0200

    Add new app.js, remove scroll

---
+++ webpage/CSS&JS/app.js
@@ -0,0 +1,13 @@
+var scrlLines = document.querySelector('.scrl');
+var navBar = document.querySelector('nav');
+
+if (window.outerWidth > 800) {
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 70) {
+            navBar.classList.add('nav-animation');
+        } else {
+            navBar.classList.remove('nav-animation');
+        }
+
+    });
+}
